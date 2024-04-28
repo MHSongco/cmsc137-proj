29435d96a4b277a85dbb18a7fa9fb5ea20f8c680
mhsongco <mhsongco@up.edu.ph>
2024-04-28 21:00:44 +0800

    added game level data

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Main {
+
+}
